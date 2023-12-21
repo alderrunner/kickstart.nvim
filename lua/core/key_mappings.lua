961606179bf64d20a087d093865937cc4e3b03a8
robin <robin.hagmayer@gmail.com>
2023-12-21 16:21:24 +0100

    Basic reconfiguration
    Deleted everything related to kickstart.nvim and added a basic
    configuration.

---
+++ lua/core/key_mappings.lua
@@ -0,0 +1,5 @@
+local keymap = vim.keymap
+
+keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/core/options.lua
@@ -0,0 +1,36 @@
+local opt = vim.opt
+
+-- Tab / Indentation
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+-- Search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+
+-- Appearance
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+opt.colorcolumn = "100"
+opt.signcolumn = "yes"
+opt.scrolloff = 10
+opt.completeopt = "menuone,noinsert,noselect"
+
+-- Behaviour
+opt.mouse = "a"
+opt.undofile = true
+opt.errorbells = false
+opt.encoding = "UTF-8"
+opt.splitright = true
+opt.splitbelow = true
+opt.updatetime = 150
+opt.timeoutlen = 200
+
+-- vim: ts=2 sts=2 sw=2 et
