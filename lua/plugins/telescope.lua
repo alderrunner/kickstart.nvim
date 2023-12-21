fefd537c23a67c01a4d27fd8cd407d1b3bd23e7c
robin <robin.hagmayer@gmail.com>
2023-12-21 20:14:18 +0100

    Added some simple plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,16 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end,
+      },
+    },
     config = function()
       local builtin = require("telescope.builtin")
 
