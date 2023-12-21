f22f02459b2580e189f3ce1d367c74967e1f7d8d
robin <robin.hagmayer@gmail.com>
2023-12-21 20:52:52 +0100

    Neo-tree can now open directories

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,12 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        hijack_netrw_behavior = "open_current",
+      },
+    })
+
     vim.keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", { silent = true, desc = "Toggle Neo-tree" })
     vim.keymap.set("n", "<leader>ef", ":Neotree focus<CR>", { silent = true, desc = "Toggle Neo-tree" })
   end
